901219cc96011a2f3c8b3d73d624c501755033d5
Sam Mestern <smestern@uwo.ca>
2023-04-16 17:39:53 -0400

    Fixed apr16th

--- demo_net_ICHIYAMA_2022.py
+++ demo_net_ICHIYAMA_2022.py
@@ -2,8 +2,8 @@ from brian2 import *
 from ni_interface.ni_brian2 import *
 import os
 import time
-seed(4323)
-defaultclock.dt = 0.1*ms
+seed(43)
+defaultclock.dt = 0.05*ms
 set_device('cpp_standalone', build_on_run=True)
 # state 1 (network-bursting): a = 400 * nS; we = 0.1 * nS; wi = 100 * nS
 # state 2 (network-reduced-inhibition): a = 400 * nS; we = 0.1 * nS; wi = 10 * nS
@@ -11,7 +11,7 @@ set_device('cpp_standalone', build_on_run=True)
 import time
 start_scope()
 start_time = time.time()
-seed(4323)
+seed(43)
 # network parameters
 N = 1e3
 p_ei = 0.02
@@ -31,9 +31,8 @@ VR = -71.40471178 * mV
 LTS = {'tauw':(92.07978488 + np.random.normal(loc=10, scale=5, size=500))*ms, 'a': np.random.normal(loc=0.090873665, scale=0.0001, size=500)*nS, 
        'b': np.abs(np.random.normal(loc=38.65665726, scale=3, size=500))*pA } # low-threshold spiking(?)
 FS = {'tauw': 13.5686673*ms, 'a': 0.*nS, 'b': 3*pA }
-#{'tauw': 144*ms, 'a': 0.1*nS, 'b': 0*nA } # fast spiking(?)
-#{'N': 1, '_run_time': 2, 'Cm': 20.21738406, 'EL': -67.44385, 'VT': -45.55570857, 'VR': -53.08817532,
-#'taum': 20.54444831, 'tauw': 13.5686673, 'a': 0.08555308306564564, 'b': 0.17, 'DeltaT': 8.543155448, 'dt': 0.1}
+
+
 # synapse parameters
 Ee = 0 * mvolt
 Ei = -80 * mvolt
@@ -50,14 +49,8 @@ taubr = 8 * second
 
 
 
-#sin_ = np.sin(0.005 * np.arange(0, 2500)) * 10 * pA
-#sin_ = (chirp(np.arange(0, 10, 0.001),f0=0,t1=10,f1=3) * 1)* pA
-#input_curr = TimedArray(sin_, dt=1*ms)
-# 
-#-pr*(1./taup) 
 # Membrane Equation + y*(Ee-v)
 eqs = Equations('''
-dv/dt = ( gL*(EL-v) + gL*DeltaT*exp((v - VT)/DeltaT) + ge*(Ee-v) + gi*(Ei-v) - w) * (1./C) : volt (unless refractory)
 d_I = clip(( ge*(Ee-v) + gi*(Ei-v)), -250*pA, 250*pA) : amp 
 dw/dt = ( a*(v - EL) - w ) / tauw : amp 
 dgi/dt = -gi*(1./taui) : siemens
@@ -82,31 +75,37 @@ input_3 : 1
 VR : volt
 v_gap : volt''')
 
+membrane_eq = Equations('''dv/dt = ( gL*(EL-v) + gL*DeltaT*exp((v - VT)/DeltaT) + ge*(Ee-v) + gi*(Ei-v) - w) * (1./C) : volt (unless refractory)''')
+vitro_eq = Equations('''v : volt''')
+
 
 # build network
-P = NeuronGroup( N, model=eqs, threshold='v>Vcut', reset='v=VR; w+=br', refractory='refrac', method='euler')
-P.pr = 1
-P.VT = VT
-P.VR = VR
-P.Vcut = (VT + 5 * DeltaT )
-P.taui = taui
+P = NeuronGroup( N//2, model=eqs + membrane_eq , threshold='v>Vcut', reset='v=VR; w+=br', refractory='refrac', method='euler')
+
+P2 = NeuronGroup( N//2, model=eqs + vitro_eq, threshold='v>Vcut', reset='v=VR; w+=br', refractory='refrac', method='euler')
 #print(P.Vcut)
-CRH = P[:int(floor(0.5*N))]; GABA = P[int(floor(0.5*N)):]
+P.pr = 1; P2.pr = 1
+P.VT = VT; P2.VT = VT
+P.VR = VR; P2.VR = VR
+P.Vcut = (VT + 5 * DeltaT ); P2.Vcut = (VT + 5 * DeltaT )
+P.taui = taui;       
+CRH = P2; GABA = P
 CRH.tauw = LTS['tauw']; CRH.a = LTS['a']; CRH.b = LTS['b']
 GABA.tauw = FS['tauw']; GABA.a = FS['a']; GABA.b = FS['b']
 GABA.taue = tauCRH; CRH.taue = taue
 GABA.refrac = 1*ms
 CRH.refrac = (1 + np.abs(np.random.normal(loc=1.5, scale=1, size=500))) *ms
 
-neuron = P[:1]
+neuron = P2
 neuron.run_regularly(f'v = step_clamp(t, d_I*int(t/second>3))', dt=defaultclock.dt)
 neuron.Vcut = 0*mV
 
 
-add_neuron = P[1:500]
-add_neuron.run_regularly(f'v = v_gap + 5*mV*randn()', dt=defaultclock.dt)
-gap_syn = Synapses(neuron, add_neuron, '''v_gap_post = v_pre : volt (summed)''', method='euler')
-gap_syn.connect()
+
+#add_neuron = P[1:500]
+#add_neuron.run_regularly(f'v = v_gap + 5*mV*randn()', dt=defaultclock.dt)
+#gap_syn = Synapses(neuron, add_neuron, '''v_gap_post = v_pre : volt (summed)''', method='euler')
+#gap_syn.connect()
 #neuron.r
 #CRH[:1].Rrsetter.up
 #presynaptic indices
@@ -127,7 +126,7 @@ P.input_3 = 0
 # poisson input
 input = []
 sub = []
-input=PoissonInput(CRH, 'ge', 1, 50.53785123*Hz, weight='we*input_1')
+#input=PoissonInput(CRH, 'ge', 1, 50.53785123*Hz, weight='we*input_1')
 input2 = PoissonInput( CRH, 'ge', 1, 50*Hz, weight='we*input_2')
 input3 = PoissonInput( CRH, 'ge', 1, 75*Hz, weight='we*input_3')
 #input2 = PoissonInput( CRH, 'gi', 1, 1*Hz, weight=wi )
@@ -148,13 +147,13 @@ Mv2 = StateMonitor(GABA, 'v', record=GABA_TO_0)
 M_crossings = SpikeMonitor(CRH)
 #run(2*second, report='text')
 
-device = init_neuron_device(device=device, dt=defaultclock.dt, scalefactor_out=2.5)
+device = init_neuron_device(device=device, dt=defaultclock.dt)
 
 
 
 time_start = time.time()
 run(60*second, report='text')
-#print(time.time() - time_start)
+print(time.time() - time_start)
 subplot(211)
 plot(Mv.t/ms, Mv.v[0]/mV, label='v', c='k', alpha=0.5)
 spike_dict = M_crossings.spike_trains()
@@ -166,5 +165,13 @@ subplot(212)
 for i,_ in enumerate(GABA_TO_0):
     plot(Mv2.t/ms, Mv2.v[i]/mV, label='v', c='k', alpha=0.5)
 show()
+id = int(np.random.randint(0, 999) * time.time())
+#import joblib and save the monitors with a unique id
+import joblib
+joblib.dump(Mv.v, f'Mv_{id}.pkl')
+joblib.dump(Mv2.v, f'Mv2_{id}..pkl')
+joblib.dump(spike_dict, f'M_crossings_{id}.pkl')
+
+
 
 print('p')
